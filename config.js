ec18feaa7fe31c5f55ec9a5e15f473505b1057bd
César Mora <cesar.mcid@gmail.com>
2020-06-24 15:24:11 -0400

    Cambiando rutas absolutas a relativas

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 module.exports = {
-  saveLocation: '/Users/cmora/Desktop/CanvasData/dataFiles', // This is my route, change it
-  unpackLocation: '/Users/cmora/Desktop/CanvasData/unpackedFiles', // This is my route, change it
+  saveLocation: './dataFiles', 
+  unpackLocation: './unpackedFiles',
   apiUrl: 'https://api.inshosteddata.com/api',
   key: "<< YOUR CANVAS KEY >>",
   secret: "<< YOUR CANVAS SECRET >>",
